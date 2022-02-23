db63ce64eb604c7ac9fba48a558dd828bfd7dc2e
Jérémie Nuel <jeremie.nuel@pm.me>
2022-02-23 13:39:23 +0100

    reprise de js

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 gs -sDEVICE=pdfwrite -dBATCH -dCompatibilityLevel=1.4 -sProcessColorModel=DeviceGray -sColorConversionStrategy=Gray -dPDFSETTINGS=/screen -dQUIET -dNOPAUSE -sOutputFile=book201221bd.pdf book201221.pdf
 
-gs -sDEVICE=pdfwrite -dBATCH -dCompatibilityLevel=1.4 -sProcessColorModel=DeviceGray -sColorConversionStrategy=Gray -dPDFSETTINGS=/prepress -dQUIET -dNOPAUSE -sOutputFile=document.pdf Document.pdf
+gs -sDEVICE=pdfwrite -dBATCH -dCompatibilityLevel=1.3 -sProcessColorModel=DeviceGray -sColorConversionStrategy=Gray -dPDFSETTINGS=/prepress -dQUIET -dNOPAUSE -sOutputFile=document.pdf Document.pdf
+
+
+pagedjs-cli http://localhost:5000/ -o document.pdf
--- static/dev/index.js
+++ static/dev/index.js
@@ -24,40 +24,70 @@ class imageRatio extends Paged.Handler {
       })
       // when the image loads
       img.onload = function () {
+
+        /* on calcule la taille de l'image et les marges en fonction de la page */
+
         // page in px 
-        const pageH = 500
-        const pageW = 300
+        const pageH = 480
+        const pageW = 320
 
         // find its height
-        const imH = img.height
+        const imH = img.naturalHeight
 
         // find its width
-        const imW = img.width
+        const imW = img.naturalWidth
         
         const imageHeightPage = (imH / 100) * 6
+        const imageWidthPage = (imW / 100) * 6
 
-        let ratio = Math.floor(imH / imW)
-        
-        // TODO: ajouter le ratio
-        const marginImage = ((pageH - imageHeightPage) / 2)
-        if (marginImage > 0 && ratio === 2) {
-          image.style.marginTop = marginImage + 'px'
-        } else if (marginImage > 0 && ratio === 0) {
-          image.style.marginTop = marginImage + 'px'
-        } else {
-          image.style.marginTop = null
+        const ratio = Math.floor(imH / imW)
+
+        const marginH = (pageH - imageHeightPage)/2
+        const marginW = (pageW - imageWidthPage)/2
+
+        if (ratio === 0) {
+          image.style.marginLeft = marginW + 'px' 
+          image.style.marginTop = (marginH - 30) + 'px'
+          image.parentNode.style.height = pageH + 'px'
+          image.parentNode.style.width = pageW + 'px'
+          image.style.width = '80%'
         }
         
-        if (imH > 6000) {
-          image.parentNode.parentNode.classList.add('imageA')
-        } else if (imH > 2000 && imH < 6000) {
-          image.parentNode.parentNode.classList.add('imageB')
-        } else {
-          image.parentNode.parentNode.classList.add('imageC')
+        
+        if (ratio === 1) {
+          image.style.marginLeft = marginW + 'px' 
+          image.style.marginTop = (marginH - 50) + 'px'
+          image.parentNode.style.height = pageH + 'px'
+          image.parentNode.style.width = pageW + 'px'
+          image.style.width = '70%'
         }
-          
-        console.log(img, imH, ratio)
-      
+        
+        
+        if (ratio === 2) {
+          image.style.marginLeft = (marginW + 30) + 'px' 
+          image.style.marginTop = (marginH + 30) + 'px'
+          image.parentNode.style.height = pageH + 'px'
+          image.parentNode.style.width = pageW + 'px'
+          image.style.height = '75%'
+        }
+
+        if (ratio === 3) {
+          image.style.marginLeft = (marginW + 50) + 'px' 
+          image.style.marginTop = (marginH + 50) + 'px'
+          image.parentNode.style.height = pageH + 'px'
+          image.parentNode.style.width = pageW + 'px'
+          image.style.height = '80%'
+        }
+        
+
+
+
+
+        // image.style.width = imageWidthPage + 'px'
+        // image.style.height = imageHeightPage + 'px'
+        // image.style.height = '480px'
+        
+        console.log(marginW, marginH, ratio)
 
         // resolve the promise
         resolve()
